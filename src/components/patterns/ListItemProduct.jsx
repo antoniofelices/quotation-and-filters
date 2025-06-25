43257b1fa15b548be3e7c07cf34a2dab3d53a59a
antoniofelices <antonio@studiomoare.com>
2025-06-25 20:58:41 +0200

    Products components

--- src/components/patterns/ListItemProduct.jsx
+++ src/components/patterns/ListItemProduct.jsx
@@ -1,27 +1,23 @@
-// import { useState } from 'react'
-
-const ListItemProduct = ({ content }) => {
-    // const [toggle, setToggle] = useState(false)
-
-    // const handleClick = () => {
-    //     setToggle(!toggle)
-    // }
-
+const ListItemProduct = ({ content, isChecked, checkHandler }) => {
     return (
         <>
-            {/* <div className="mx-8 my-4 flex items-center justify-between gap-3 rounded-lg bg-white p-10 shadow-lg shadow-gray-300/100 lg:mx-0"> */}
-            <div>
-                <h2 className="text-lg font-bold text-green-900">
-                    {content.title}
-                </h2>
-                <p className="text-sm">{content.text}</p>
+            <div className="mx-8 my-4 flex items-center justify-between gap-3 rounded-lg bg-white p-10 shadow-lg shadow-gray-300/100 lg:mx-0">
+                <div>
+                    <h2 className="text-lg font-bold text-green-900">
+                        {content.title}
+                    </h2>
+                    <p className="text-sm">{content.text}</p>
+                </div>
+                <p>{content.price}</p>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={checkHandler}
+                    />
+                    Add
+                </label>
             </div>
-            <p>{content.price}</p>
-            {/* <label>
-                <input type="checkbox" onClick={handleClick} />
-                Add
-            </label> */}
-            {/* </div> */}
         </>
     )
 }
